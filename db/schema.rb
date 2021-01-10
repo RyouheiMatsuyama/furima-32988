0ae8d3ebfe8133dd6f8e37246b09d6b33f509013
RyouheiMatsuyama <ryohei.0427@ezweb.ne.jp>
2021-01-10 11:58:13 +0900

    ユーザー管理機能　修正2

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_01_07_093752) do
+
+  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
