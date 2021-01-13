2b731078d1bc79f299bac57d49a23288657325a3
RyouheiMatsuyama <ryohei.0427@ezweb.ne.jp>
2021-01-13 18:03:51 +0900

    商品出品機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  #root to: "items#new"
-  #get 'items/new' to: "items#new"
   resources :users, only: [:index, :new, :create, :destroy]
   resources :items, only: [:index, :new, :create, ]
 end
